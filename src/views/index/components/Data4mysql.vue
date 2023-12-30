469c44640559677896a187428fa0ecfa35b30d47
kimura <tansen87@qq.com>
2023-12-30 14:37:18 +0800

    update code format

--- src/views/index/components/Data4mysql.vue
+++ src/views/index/components/Data4mysql.vue
@@ -1,85 +1,89 @@
 <script setup lang="ts">
-import { ref, reactive} from "vue";
-import { open } from "@tauri-apps/api/dialog";
-import { invoke } from "@tauri-apps/api/tauri";
-import { listen } from '@tauri-apps/api/event';
-import { ElMessage } from 'element-plus';
+  import { ref, reactive } from 'vue';
+  import { open } from '@tauri-apps/api/dialog';
+  import { invoke } from '@tauri-apps/api/tauri';
+  import { listen } from '@tauri-apps/api/event';
+  import { ElMessage } from 'element-plus';
 
-const getYamlMsg = ref("");
-const data = reactive({
-  filePath: "",
-  fileFormats: ["yaml"],
-});
-const form = reactive({
-  region: '凭证表&科目余额表',
-  input: "业务说明",
-  epath: "C:\\Users",
-})
+  const getYamlMsg = ref('');
+  const data = reactive({
+    filePath: '',
+    fileFormats: ['yaml'],
+  });
+  const form = reactive({
+    region: '凭证表&科目余额表',
+    input: '业务说明',
+    epath: 'C:\\Users',
+  });
 
-listen('check', (event: any) => {
-  const check: any = event.payload;
-  ElMessage.info(check)
-})
+  listen('check', (event: any) => {
+    const check: any = event.payload;
+    ElMessage.info(check);
+  });
 
-listen('prepareErr', (event: any) => {
-  const error: any = event.payload;
-  ElMessage.error(error)
-})
-listen('executeErr', (event: any) => {
-  const error: any = event.payload;
-  ElMessage.error(error)
-})
+  listen('prepareErr', (event: any) => {
+    const error: any = event.payload;
+    ElMessage.error(error);
+  });
+  listen('executeErr', (event: any) => {
+    const error: any = event.payload;
+    ElMessage.error(error);
+  });
 
-listen('errcode', (event: any) => {
-  const errCode: any = event.payload;
-  ElMessage.error(errCode)
-})
+  listen('errcode', (event: any) => {
+    const errCode: any = event.payload;
+    ElMessage.error(errCode);
+  });
 
-listen('message', (event: any) => {
-  const progress: any = event.payload;
-  ElMessage.info(progress)
-})
+  listen('message', (event: any) => {
+    const progress: any = event.payload;
+    ElMessage.info(progress);
+  });
 
-// download mysql data
-async function getData() {
-  if (data.filePath == "") {
-    ElMessage.warning("未选择yaml文件")
-    return;
-  } 
+  // download mysql data
+  async function getData() {
+    if (data.filePath == '') {
+      ElMessage.warning('未选择yaml文件');
+      return;
+    }
 
-  if (data.filePath != "") {
-    ElMessage.info("正在下载,请稍等...");
-    let value = await invoke("download", { filePath: data.filePath, etable: form.region, rcolumn: form.input, epath: form.epath })
+    if (data.filePath != '') {
+      ElMessage.info('正在下载,请稍等...');
+      let value = await invoke('download', {
+        filePath: data.filePath,
+        etable: form.region,
+        rcolumn: form.input,
+        epath: form.epath,
+      });
       // .then((msg: any) => ElMessage.success(msg))
       // .catch((err: any) => ElMessage.error(err))
-    console.log(value)
-    ElMessage.success("下载完成!");
+      console.log(value);
+      ElMessage.success('下载完成!');
+    }
   }
-}
 
-async function selectFile() {
-  const selected = await open({
-    multiple: false,
-    filters: [
-      {
-        name: 'Yaml',
-        extensions: data.fileFormats,
-      },
-    ]
-  });
-  if (Array.isArray(selected)) {
-    // user selected multiple files
-    data.filePath = selected.toString();
-  } else if (selected === null) {
-    // user cancelled the selection
-    return;
-  } else {
-    // user selected a single file
-    data.filePath = selected;
+  async function selectFile() {
+    const selected = await open({
+      multiple: false,
+      filters: [
+        {
+          name: 'Yaml',
+          extensions: data.fileFormats,
+        },
+      ],
+    });
+    if (Array.isArray(selected)) {
+      // user selected multiple files
+      data.filePath = selected.toString();
+    } else if (selected === null) {
+      // user cancelled the selection
+      return;
+    } else {
+      // user selected a single file
+      data.filePath = selected;
+    }
+    getYamlMsg.value = selected.toString();
   }
-  getYamlMsg.value = selected.toString();
-}
-
 </script>
 
 <template>
@@ -91,21 +95,21 @@ async function selectFile() {
       </el-select>
     </el-form-item>
     <el-form-item label="Replace col">
-      <el-input v-model="form.input" placeholder="Please input replace column"/>
+      <el-input v-model="form.input" placeholder="Please input replace column" />
     </el-form-item>
     <el-form-item label="Export path">
-      <el-input v-model="form.epath" placeholder="Please input export path"/>
+      <el-input v-model="form.epath" placeholder="Please input export path" />
     </el-form-item>
     <el-form-item>
       <el-button type="primary" @click="selectFile()">Open Yaml</el-button>
       <el-button type="success" @click="getData()">Download</el-button>
     </el-form-item>
-    </el-form>
-    <el-text class="mx-1" type="success">{{ getYamlMsg }}</el-text>
+  </el-form>
+  <el-text class="mx-1" type="success">{{ getYamlMsg }}</el-text>
 </template>
 
 <style>
-.el-input {
-  width: 500px;
-}
+  .el-input {
+    width: 500px;
+  }
 </style>
