a55210b1bd8f21be3e3f0ee8d4295218234c6308
kimura <tansen87@qq.com>
2023-12-30 14:31:42 +0800

    update code format

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -40,7 +40,7 @@
               <span>unique</span>
             </div>
           </template>
-          <Unique/>
+          <Unique />
         </el-card>
       </el-col>
       <el-col :xs="24" :sm="24" :md="12" :lg="12" :xl="12">
