c2da9bc98c3f92ac812a70fef4d10d6e36ca7aa4
kimura <tansen87@qq.com>
2024-06-01 08:19:22 +0800

    refactor(Csv2xlsx): styles

--- src/views/index/components/Excel2csv.vue
+++ src/views/index/components/Excel2csv.vue
@@ -121,7 +121,7 @@
       <el-button type="success" @click="excelTocsv()">Convert</el-button>
     </el-form-item>
   </el-form>
-  <el-table :data="selectedFiles" height="300" style="width: 100%">
+  <el-table :data="selectedFiles" height="250" style="width: 100%">
     <el-table-column prop="filename" label="file" width="480"></el-table-column>
     <el-table-column
       prop="status"
